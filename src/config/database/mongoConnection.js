34f718904351db0fe765d4dd0a5b9614a17e6261
maximilianosulsenti <maximilianosulsenti@gmail.com>
2026-02-06 15:45:54 -0300

    se mueve carpeta de database a config

---
+++ src/config/database/mongoConnection.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { env } from "../environment.js";
+
+export default class mongoSingleton {
+   static #instance;
+
+   constructor(){
+      mongoose.connect(env.MONGO_URI);
+   }
+
+   static getInstance(){
+      if(this.#instance){
+         console.log("Ya existe una instancia de MongoDB");
+         return this.#instance;
+      }
+
+      this.#instance = new mongoSingleton();
+      console.log("Se creó una nueva instancia de MongoDB");
+      return this.#instance;
+   }
+}
